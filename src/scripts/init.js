5e0c59fdde8565503010de8dea507063cc3b25e6
Alexander Ezhov <aezhov@inno.tech>
2022-03-23 17:37:11 +0300

    canvas resizing

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,14 +1,21 @@
 import BackgroundImage from "../assets/bg.jpeg";
 import { canvas } from "./utils/canvas";
 
-const init = () => {
+const resizeCanvas = ()=>{
   const viewportWidth = visualViewport.width - 40
   const viewportHeight = visualViewport.height - 40
   const minSize = Math.min(viewportWidth, viewportHeight)
   const resultSize = `${minSize}px`
-  canvas.style.background = `url(${BackgroundImage})`;
   canvas.style.width = resultSize;
   canvas.style.height = resultSize;
+}
+
+const init = () => {
+  canvas.style.background = `url(${BackgroundImage})`;
+  resizeCanvas()
 };
+
+window.addEventListener("orientationchange", resizeCanvas);
+window.addEventListener("resize", resizeCanvas);
 
 export default init;
