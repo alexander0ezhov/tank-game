1410c8cce7bf8eaeeb56f13fd36cceebead88779
Alexander Ezhov <aezhov@inno.tech>
2022-03-23 17:32:34 +0300

    canvas size initialization

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -2,7 +2,13 @@ import BackgroundImage from "../assets/bg.jpeg";
 import { canvas } from "./utils/canvas";
 
 const init = () => {
+  const viewportWidth = visualViewport.width - 40
+  const viewportHeight = visualViewport.height - 40
+  const minSize = Math.min(viewportWidth, viewportHeight)
+  const resultSize = `${minSize}px`
   canvas.style.background = `url(${BackgroundImage})`;
+  canvas.style.width = resultSize;
+  canvas.style.height = resultSize;
 };
 
 export default init;
